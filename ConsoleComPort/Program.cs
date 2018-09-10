dee982095a72240d7ba867f1b4fc705106f896e9
Chao-Wei, Peng <alanwei1202@gmail.com>
2018-09-10 22:40:04 +0800

    upload

---
+++ ConsoleComPort/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleComPort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //{4d36e978-e325-11ce-bfc1-08002be10318}為設備類別port（端口（COM&LPT））的GUID
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2",
+                    "SELECT * FROM Win32_PnPEntity WHERE ClassGuid=\"{4d36e978-e325-11ce-bfc1-08002be10318}\"");
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    string name = queryObj.GetPropertyValue("Name").ToString();
+                    string desc = queryObj.GetPropertyValue("Description").ToString();
+                    Console.WriteLine("{0} : {1}", name, desc);
+                }
+            }
+            catch (Exception ex)
+            {
+                //Log.w
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
